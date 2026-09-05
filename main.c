678466e69986bb57fcdd2b3279c385b7e71936b7
dark <dark>
2026-09-05 14:49:24 +0300

    Added motherboard name

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include "main.h"
 #include <psapi.h>
 #include <winbase.h>
+#include "winreg.h"
 
 // To ensure correct resolution of symbols, add Psapi.lib to TARGETLIBS
 // and compile with -DPSAPI_VERSION=1
@@ -16,10 +17,27 @@ void get_pc_name(device_info_t *source)
 	strcpy((*source).hardware_info.static_info.pc_name, name);
 }
 
+void get_mother_board_static_value(device_info_t *source)
+{
+	HKEY	key;
+	char	label_name[128];
+	char	model_name[128];
+	DWORD	buffer_size = sizeof(label_name);
+	RegOpenKeyExA(HKEY_LOCAL_MACHINE, "HARDWARE\\DESCRIPTION\\System\\BIOS", 0, KEY_READ, &key);
+	RegQueryValueExA(key, "BaseBoardManufacturer", NULL, NULL, (LPBYTE)label_name, &buffer_size);
+	RegQueryValueExA(key, "BaseBoardProduct", NULL, NULL, (LPBYTE)model_name, &buffer_size);
+	source.hardware_info.static_info.motherboard_label = label_name;
+	source.hardware_info.static_info.motherboard_name = model_name;
+	printf("%s\n", label_name);
+	printf("%s\n", model_name);
+
+}
+
 int main( void )
 {
 	device_info_t device_info;
 	get_pc_name(&device_info);
+	get_mother_board_static_value(&device_info);
 	printf("%s\n", device_info.hardware_info.static_info.pc_name);
     // Get the list of process identifiers.
 	//pc_hardware_info_static_t static_hardware_info;
